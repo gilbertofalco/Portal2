0c39a7aa480793e3955b86f96977d28f1e38eb22
gilbe <gilbe@192.168.0.4>
2019-06-30 21:18:45 -0300

    Merge origin/master

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -13,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -39,7 +40,15 @@ public class LoginController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        btnEntrar.setOnMouseClicked((MouseEvent e) -> {
+            System.out.println("Entrou");
+        });
+        btnCancelar.setOnMouseClicked((MouseEvent e) -> {
+            System.out.println("Cancelou");
+        });
         // TODO
     }    
+
     
 }
