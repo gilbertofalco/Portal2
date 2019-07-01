8d588ac5fdb9b97d8c80d63d5343292ffe1d1d7d
gilbe <gilbe@192.168.0.4>
2019-06-30 23:37:14 -0300

    new commit

--- src/DAO/CandidatoDao.java
+++ src/DAO/CandidatoDao.java
@@ -5,8 +5,13 @@
  */
 package DAO;
 
-import JDBC.ConnectionFactory
+import JDBC.ConnectionFactory;
+import Model.Candidato;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +24,32 @@ public class CandidatoDao {
         this.con = new ConnectionFactory().getConnection();
     }
     
-    public boolean add(Candidato a){
+    public boolean add(Candidato c){
         String sql = "INSERT INTO pessoa(id, nome, senha, email, telefone, celular,"
-                + " endereco, bairro, cidade, estado, site, curso)"
-                + "
+                + " endereco, bairro, cidade, estado, site, curso) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, Long.toString(c.getCodigo()));
+            stmt.setString(2, c.getNome());
+            stmt.setString(3, c.getSenha());
+            stmt.setString(4, c.getEmail());
+            stmt.setString(5, c.getTelefone());
+            stmt.setString(6, c.getCelular());
+            stmt.setString(7, c.getEndereco());
+            stmt.setString(8, c.getBairro());
+            stmt.setString(9, c.getCidade());
+            stmt.setString(10, c.getEstado());
+            stmt.setString(11, c.getSite());
+            stmt.setString(12, c.getCurso());
+            stmt.execute();
+            
+            return true;
+            
+        } catch (SQLException ex){
+            Logger.getLogger(CandidatoDao.class.getName()).log(Level.SEVERE, null, sql);
+            return false;            
+        }
+    }
+}
+    
